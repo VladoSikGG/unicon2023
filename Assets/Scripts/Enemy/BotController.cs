ce688040008aaebbbdb7c20daffa25b082e17d48
ACTPOHOM <mr.a.zhar@mail.ru>
2023-04-25 10:32:31 +0300

    add scripts to Bot

---
+++ Assets/Scripts/Enemy/BotController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class BotController : BotInterface
+{
+    private void Update()
+    {
+        float dis = Vector3.Distance(target.position, transform.position);
+
+
+        //update for new project
+        if (target == null)
+            return;
+        if (dis <= distance && dis > distanceForAttake)
+        {
+            EnemyWalk(target.position);
+        }
+        else if (dis > distance)
+        {
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+                GotoNextPoint();
+        }
+        else if (dis <= distanceForFastAttake)
+        {
+            RotateToTarget();
+            EnemyAttack();
+        }
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+    }
+}
