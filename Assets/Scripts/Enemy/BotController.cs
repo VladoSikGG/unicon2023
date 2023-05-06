0ee5224a24a3ae71cd8c2af6ee83c192e624f79a
RUDIK <vladislavrudakovskij@gmail.com>
2023-05-06 20:20:33 +0300

    Добавил анимацию на ботов
    -подключены в коде
    -скачаны в папку

--- Assets/Scripts/Enemy/BotController.cs
+++ Assets/Scripts/Enemy/BotController.cs
@@ -6,27 +6,37 @@ using static UnityEngine.GraphicsBuffer;
 
 public class BotController : BotInterface
 {
+    private Animation _anim;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animation>();
+    }
     private void Update()
     {
         float dis = Vector3.Distance(target.position, transform.position);
-
-
         //update for new project
         if (target == null)
             return;
         if (dis <= distance && dis > distanceForAttake)
         {
             EnemyWalk(target.position);
+            _anim.Play("Sprint Forward");
         }
         else if (dis > distance)
         {
             if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                _anim.Play("Rifle Walk");
                 GotoNextPoint();
+            }
+                
         }
         else if (dis <= distanceForFastAttake)
         {
             RotateToTarget();
             EnemyAttack();
+            _anim.Play("Firing Rifle");
         }
 
         Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
